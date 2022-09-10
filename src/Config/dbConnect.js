36c4e6593a69022ad8f4b21bd4d9f31388c6a04c
Caio Sergio <caioseal@gmail.com>
2022-09-10 17:40:35 -0300

    Add files via upload

--- src/Config/dbConnect.js
+++ src/Config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
-
-require('dotenv').config()
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 mongoose.connect(process.env.MONGODB_URI)
 
